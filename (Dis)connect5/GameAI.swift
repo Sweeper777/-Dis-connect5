2a674131a1562a68ce3c9299c5140c434cfcc3f4
Sweeper777 <sumulang@gmail.com>
2018-07-03 08:56:29 +0100

    added evaluateHeuristics

--- (Dis)connect5/GameAI.swift
+++ (Dis)connect5/GameAI.swift
@@ -14,4 +14,17 @@ class GameAI {
         self.mySide = mySide
     }
     
+    func evaluateHeuristics() -> Int {
+        if let winner = game.checkWinner() {
+            return winner == mySide ? 10000 : -10000
+        }
+        let analysedResult = game.getRows().map(RowAnalyser.init).map { $0.analyse() }.reduce(0, +)
+        switch mySide {
+        case .connect:
+            return analysedResult
+        case .disconnect:
+            return 1000 - analysedResult
+        }
+    }
+    
 }
